5ea8bbaef368ac8da4dfb4eaba78c4e1e267f480
i01er <free2737@gmail.com>
2018-09-29 13:35:01 +0800

    Add files via upload

---
+++ LittleBook.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.4.11;
+
+/*
+LittleBook
+ -- Expenses Keeper
+----------
+5 Aug 2018
+iDler
+*/
+
+contract Ownable
+{
+	address public owner;
+
+	function Ownable()
+	{
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner()
+	{
+		require(msg.sender == owner);
+		_;
+	}
+
+	function transferOwnership(address newOwner) onlyOwner
+	{
+		if(newOwner != address(0))
+		{
+			owner = newOwner;
+		}
+	}
+}
+
+contract AccessControl
+{
+	event ContractUpgrade(address newContract);
+
+	address public ceoAddress;
+	address public cfoAddress;
+	address public cooAddress;
+
+	bool public paused = false;
+	bool public AppEnd = false;
+
+
+}
+
+contract LittleBook
+{
+	address public Owner;
+	mapping(address => User) public Users;
+
+	uint public Version;
+	
+
+	struct Expense
+	{
+		uint    UnitNum;
+		string	UnitName;
+		uint	UnitExpense;
+	}
+
+	uint public TotalExpense;
+
+	function StartApp()
+	{
+		owner = msg.sender;
+		//AppEnd = 0;
+	}
+
+	function Authorize(address User)
+	{
+		require(msg.sender == Owner);
+		//require(Players[Player] == null);
+	}
+
+	function CreateLittleBook()
+	{
+		require(AppEnd != true);
+		require(msg.sender == User)
+
+		BookName = msg.sender;
+		Expense[] public BookName;
+	}
+
+	function AddExpense()
+	{
+		require(AppEnd != true);
+		push
+	}
+
+	function DelExpense()
+	{
+		require(AppEnd != true);
+		if(isEmpty(Expense) == 0)pull
+	}
+
+	function EndApp()
+	{
+		require(msg.sender == Owner);
+		AppEnd = true;
+
+	}
+}
